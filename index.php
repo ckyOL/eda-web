889d5a7ef02004dc7ac040f7b8e5007b085f44a0
Chen Ke Yu <gcky94@gmail.com>
2016-05-16 22:30:25 +0800

    init project

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css"  media="screen,projection"/>
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+
+<nav>
+    <div class="nav-wrapper light-blue darken-1">
+        <a class="brand-logo ">EDA</a>
+
+        
+
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="badges">Sign in</a></li>
+            <li><a href="buttons">Login in</a></li>
+        </ul>
+    </div>
+
+</nav>
+
+<div class="container">
+
+</div>
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+</body>
+</html>
