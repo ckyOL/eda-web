226734dc89eef3a728b08df8c27632d2d6cd56b2
Chen Ke Yu <gcky94@gmail.com>
2016-05-18 21:09:27 +0800

    add login card

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by chen on 16/5/18.
+ */
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal({
+        dismissible: true
+    });
+
+});
--- index.php
+++ index.php
@@ -21,8 +21,11 @@
 
         <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><a href="badges">Sign in</a></li>
-            <li><a href="buttons">Login in</a></li>
+            <li><a class="modal-trigger" href="#login">Login in</a></li>
+
         </ul>
+
+
     </div>
 
 </nav>
@@ -30,8 +33,20 @@
 <div class="container">
 
 </div>
+
+<div id="login" class="modal">
+    <div class="modal-content">
+        <h4>Modal Header</h4>
+        <p>A bunch of text</p>
+    </div>
+    <div class="modal-footer">
+        <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">同意</a>
+    </div>
+</div>
 <!--Import jQuery before materialize.js-->
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+
+<script type="text/javascript" src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+<script type="text/javascript" src="app.js"></script>
 </body>
 </html>
