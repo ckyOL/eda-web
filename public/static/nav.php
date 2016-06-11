a20cdd8a77b6773a992cdc7f50750caac483faa2
piekey1994 <xiaoshendidi@163.com>
2016-06-11 10:00:33 +0800

    update nav ,make it more adapt to mobile phones

--- public/static/nav.php
+++ public/static/nav.php
@@ -1,11 +1,12 @@
 <nav>
     <div class="nav-wrapper light-blue darken-1">
         <ul class="left">
-            <li><a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="material-icons">menu</i></a></li>
+            <li><a href="#" data-activates="slide-out" class="button-collapse show-on-large hide-on-med-and-down"><i class="material-icons">menu</i></a></li>
         </ul>
-        <a class="brand-logo hide-on-med-and-down" href="/">EDA</a>
+        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+        <a class="brand-logo" href="/">EDA</a>
 
-        <ul class="right ">
+        <ul class="right hide-on-med-and-down">
             <li><a class="modal-trigger" href="#searchmodal"><i class="material-icons left">search</i>Search Collocation</a></li>
             <li><a href="/myself"><img class="left circle" width="40" height="40" style="margin: 10px 10px" src="<?php echo $pictureUrl; ?>"><?php echo $userName; ?></a></li>
             <li><a href="/quit"><i class="material-icons left">exit_to_app</i>Quit</a></li>
@@ -16,6 +17,16 @@
             <li><a href="/friend">Friend</a></li>
             <li><a href="/notification">Notification</a></li>
             <li><a href="/setting">Setting</a></li>
+        </ul>
+        <ul class="side-nav" id="mobile-demo">
+            <li><a href="/">Hot</a></li>
+            <li><a href="/collect">Collection</a></li>
+            <li><a href="/friend">Friend</a></li>
+            <li><a href="/notification">Notification</a></li>
+            <li><a href="/setting">Setting</a></li>
+            <li><a class="modal-trigger" href="#searchmodal">search</a></li>
+            <li><a href="/myself"><?php echo $userName; ?></a></li>
+            <li><a href="/quit">Quit</a></li>
         </ul>
 
     </div>
